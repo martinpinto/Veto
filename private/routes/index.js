418de2615388c07026a4998d7a1c5401994fe7fc
Martin Pinto-Bazurco <martinpintob@gmail.com>
2015-08-04 19:33:53 +0200

    Fixed bug when redirecting connection would still be open

--- private/routes/index.js
+++ private/routes/index.js
@@ -10,17 +10,20 @@ router.get('/', function (req, res, next) {
   res.render('index', {
     title: config.get("engine.name")
   });
+  res.end();
 });
 
 /* GET authors main page. */
 router.get('/authors', function (req, res, next) {
   //currentAPIVersion = config.get('engine.currentAPIVersion');
   res.render('authors');
+  res.end();
 });
 
 /* GET authors submit page. */
 router.get('/authors/submit', function (req, res, next) {
   res.render('submitAuthors');
+  res.end();
 });
 
 /* GET quotes main page. */
@@ -31,23 +34,25 @@ router.get('/quotes', function (req, res, next) {
   } else {
     res.render('quotes');
   }
+  res.end();
 });
 
 /* GET quote submit page. */
 router.get('/quotes/submit', function (req, res, next) {
-  res.render('submitQuotes', {
-  });
+  res.render('submitQuotes');
+  res.end();
 });
 
 /* GET topics main page. */
 router.get('/topics', function (req, res, next) {
   res.render('topics');
+  res.end();
 });
 
 /* GET topics submit page. */
 router.get('/topics/submit', function (req, res, next) {
-  res.render('submitTopics', {
-  });
+  res.render('submitTopics');
+  res.end();
 });
 
 module.exports = router;
