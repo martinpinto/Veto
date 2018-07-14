f78ec4e38a4a5b51a535caf67923fc8c39be12ce
Martin Pinto-Bazurco <martinpintob@gmail.com>
2018-07-14 20:11:20 +0200

    Removed log from jwt.service.

--- server/src/shared/services/jwt.service.ts
+++ server/src/shared/services/jwt.service.ts
@@ -3,7 +3,7 @@ import * as expressJwt from 'express-jwt';
 import { Config } from '../../config';
 
 const config = Config();
-console.log(config)
+
 export const jwtService = expressJwt({
     secret: config.oauth.secret,
 });
