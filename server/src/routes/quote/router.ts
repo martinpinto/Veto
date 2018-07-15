a9688994b3666642139dad00e0f56c4c3e5e14fb
Martin Pinto-Bazurco <martinpintob@gmail.com>
2018-07-15 23:06:16 +0200

    Removed /quote from jwt.

--- server/src/routes/quote/router.ts
+++ server/src/routes/quote/router.ts
@@ -4,15 +4,13 @@ import { controller } from './quote.controller';
 
 const router = express.Router();
 
-router.use(jwtService);
-
 router.get('/', controller.getAllAction);
 router.get('/:id', controller.getOneAction);
 //router.get('/weekly', controller.getWeeklyAction);
 // router.get('/filter', this.getFilteredAction);
-router.post('/', controller.createAction);
-router.put('/:id/vote', controller.upOrDownVoteQuoteAction);            
-router.post('/:id/favorite', controller.createQuoteUserFavoriteAction);
-router.delete('/:id', controller.deleteAction);
+router.post('/', jwtService, controller.createAction);
+router.put('/:id/vote', jwtService, controller.upOrDownVoteQuoteAction);            
+router.post('/:id/favorite', jwtService, controller.createQuoteUserFavoriteAction);
+router.delete('/:id', jwtService, controller.deleteAction);
 
 export { router };
